92049f3500045f6fe12da8e63a8e07082b6bb946
RemyByeklo69 <iownuman1@hotmail.com>
2024-07-22 00:13:10 +0200

    Add files via upload

--- cob-test.py
+++ cob-test.py
@@ -2,94 +2,46 @@ import requests
 import time
 import random
 
+bearer = ""
+
+def get_next_hunt_time():
+    url = "https://bloodlore-chronicles.com/api/ClientUser/getme"
+
+    header = {
+        'ApiKey': '3DB373CF32AC02834ED4A388F512980EF3D8ED3FEF92640F6DC3B2C20DEB2935',
+        'Authorization': 'Bearer {}'.format(bearer),
+    }
+
+    response = requests.request("GET", url, headers=headers)
+
+    next_hunt_time = response.json()['nextAllowedHuntTimeStamp']
+
+    return next_hunt_time
+
+def get_new_bearer():
+    url = "https://bloodlore-chronicles.com/api/user/login"
+
+    payload={'username': 'Keurbz',
+        'password': 'b5a5e93992d49895d87efa09f26e10eec80f2878f72eb7b4f0af542e4596181d'}
+
+    headers = {
+        'ApiKey': '3DB373CF32AC02834ED4A388F512980EF3D8ED3FEF92640F6DC3B2C20DEB2935',
+        }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    new_bearer = response.json()['access_token']
+
+    return new_bearer
+
 url_1 = "https://bloodlore-chronicles.com/api/Game/hunt"
-payload_1={}
-headers_1 = {
-  'Accept': '*/*',
-  'Accept-Language': 'en-US,en;q=0.9',
-  'ApiKey': '3DB373CF32AC02834ED4A388F512980EF3D8ED3FEF92640F6DC3B2C20DEB2935',
-  'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2Njg3MTY1ZmMzNDliYjkyODBjM2NmNjQiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJ1c2VyIiwibmJmIjoxNzIxNTE5ODgwLCJleHAiOjE3MjE2MDYyODAsImlzcyI6IlNwb29reSBIdW50ZXJzIiwiYXVkIjoiU3Bvb2t5IEh1bnRlcnMifQ.H9PVumTxe1jAH7GrJi7j4V5BirjIKzUKdSwYVJu7N5c',
-  'Connection': 'keep-alive',
-  'Cookie': '_ga=GA1.1.199610205.1720025946; gtc_cookie=true; _ga_WT859GPG6Q=GS1.1.1720025946.1.1.1720025958.0.0.0; isPlaying=false; volume=0.2',
-  'Referer': 'https://bloodlore-chronicles.com/hunt',
-  'Sec-Fetch-Dest': 'empty',
-  'Sec-Fetch-Mode': 'cors',
-  'Sec-Fetch-Site': 'same-origin',
-  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
-  'sec-ch-ua': '"Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"',
-  'sec-ch-ua-mobile': '?0',
-  'sec-ch-ua-platform': '"Windows"'
-}
-
 url_2 = "https://bloodlore-chronicles.com/api/Game/quickhunt"
-
-payload_2={}
-headers_2 = {
-  'Accept': '*/*',
-  'Accept-Language': 'en-US,en;q=0.9',
-  'ApiKey': '3DB373CF32AC02834ED4A388F512980EF3D8ED3FEF92640F6DC3B2C20DEB2935',
-  'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2Njg3MTY1ZmMzNDliYjkyODBjM2NmNjQiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJ1c2VyIiwibmJmIjoxNzIxNTE5ODgwLCJleHAiOjE3MjE2MDYyODAsImlzcyI6IlNwb29reSBIdW50ZXJzIiwiYXVkIjoiU3Bvb2t5IEh1bnRlcnMifQ.H9PVumTxe1jAH7GrJi7j4V5BirjIKzUKdSwYVJu7N5c',
-  'Connection': 'keep-alive',
-  'Cookie': '_ga=GA1.1.199610205.1720025946; gtc_cookie=true; _ga_WT859GPG6Q=GS1.1.1720025946.1.1.1720025958.0.0.0; isPlaying=false; volume=0.2',
-  'Referer': 'https://bloodlore-chronicles.com/hunt',
-  'Sec-Fetch-Dest': 'empty',
-  'Sec-Fetch-Mode': 'cors',
-  'Sec-Fetch-Site': 'same-origin',
-  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
-  'sec-ch-ua': '"Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"',
-  'sec-ch-ua-mobile': '?0',
-  'sec-ch-ua-platform': '"Windows"'
-}
-
-url_3 = "https://bloodlore-chronicles.com/api/clientshop/GetWeaponMaster"
-
-payload_3={}
-headers_3 = {
-  'Accept': '*/*',
-  'Accept-Language': 'en-US,en;q=0.9',
-  'ApiKey': '3DB373CF32AC02834ED4A388F512980EF3D8ED3FEF92640F6DC3B2C20DEB2935',
-  'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2Njg3MTY1ZmMzNDliYjkyODBjM2NmNjQiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJ1c2VyIiwibmJmIjoxNzIxNTE5ODgwLCJleHAiOjE3MjE2MDYyODAsImlzcyI6IlNwb29reSBIdW50ZXJzIiwiYXVkIjoiU3Bvb2t5IEh1bnRlcnMifQ.H9PVumTxe1jAH7GrJi7j4V5BirjIKzUKdSwYVJu7N5c',
-  'Connection': 'keep-alive',
-  'Cookie': '_ga=GA1.1.199610205.1720025946; gtc_cookie=true; isPlaying=false; volume=0.2; _ga_WT859GPG6Q=GS1.1.1720025946.1.1.1720027011.0.0.0',
-  'Referer': 'https://bloodlore-chronicles.com/weaponmaster',
-  'Sec-Fetch-Dest': 'empty',
-  'Sec-Fetch-Mode': 'cors',
-  'Sec-Fetch-Site': 'same-origin',
-  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
-  'sec-ch-ua': '"Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"',
-  'sec-ch-ua-mobile': '?0',
-  'sec-ch-ua-platform': '"Windows"'
-}
-
-headers_4 = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
-    "Accept": "*/*",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Accept-Language": "en-US,en;q=0.9",
-    "ApiKey": "3DB373CF32AC02834ED4A388F512980EF3D8ED3FEF92640F6DC3B2C20DEB2935",
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2Njg3MTY1ZmMzNDliYjkyODBjM2NmNjQiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJ1c2VyIiwibmJmIjoxNzIxNTE5ODgwLCJleHAiOjE3MjE2MDYyODAsImlzcyI6IlNwb29reSBIdW50ZXJzIiwiYXVkIjoiU3Bvb2t5IEh1bnRlcnMifQ.H9PVumTxe1jAH7GrJi7j4V5BirjIKzUKdSwYVJu7N5c",
-    "Origin": "https://bloodlore-chronicles.com",
-    "Referer": "https://bloodlore-chronicles.com/jewel",
-    "Sec-Fetch-Dest": "empty",
-    "Sec-Fetch-Mode": "cors",
-    "Sec-Fetch-Site": "same-origin",
-    "Version": "0.1.0",
-    "sec-ch-ua": '"Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"',
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": '"Windows"',
-    "Content-Type": "multipart/form-data; boundary=----WebKitFormBoundaryoNbB6DKnrZDDtTV7"
-}
-
-cookies_4 = {
-    "_ga": "GA1.1.199610205.1720025946",
-    "gtc_cookie": "true",
-    "isPlaying": "false",
-    "volume": "0.2",
-    "_ga_WT859GPG6Q": "GS1.1.1720025946.1.1.1720029256.0.0.0"
-}
+url_3 = "https://bloodlore-chronicles.com/api/game/buysell"
 
 # Define the body content
-body_4 = (
+# pearl: 64ecd734a77efbd2ce8cca15
+# jasper: 660eb31b5273f51a9e2eebb4
+buy_body = (
     "------WebKitFormBoundaryoNbB6DKnrZDDtTV7\r\n"
     "Content-Disposition: form-data; name=\"itemId\"\r\n\r\n"
     "64ecd734a77efbd2ce8cca15\r\n"
@@ -99,26 +51,42 @@ body_4 = (
     "------WebKitFormBoundaryoNbB6DKnrZDDtTV7--\r\n"
 )
 
-# pearl: 64ecd734a77efbd2ce8cca15
-# jasper: 660eb31b5273f51a9e2eebb4
+buy_counter = 0
+bearer = get_new_bearer()
 
-buy_counter = 5
+header = {
+  'ApiKey': '3DB373CF32AC02834ED4A388F512980EF3D8ED3FEF92640F6DC3B2C20DEB2935',
+  'Authorization': 'Bearer {}'.format(bearer),
+}
 
 while True:
-    buy_counter += 1
-    hunt_response = requests.request("GET", url_1, headers=headers_1, data=payload_1)
-    print("Hunt status code: {}".format(hunt_response.status_code))
+    # buy_counter += 1
+    hunt_response = requests.request("GET", url_1, headers=header)
+    hunt_status_code = hunt_response.status_code
+    print("Hunt status code: {}".format(hunt_status_code))
+    if hunt_status_code != 200:
+        bearer = get_new_bearer()
+        header = {
+            'ApiKey': '3DB373CF32AC02834ED4A388F512980EF3D8ED3FEF92640F6DC3B2C20DEB2935',
+            'Authorization': 'Bearer {}'.format(bearer),
+        }
     time.sleep(random.randint(4, 8))
-    hunt_response = requests.request("GET", url_2, headers=headers_2, data=payload_2)
+    hunt_response = requests.request("GET", url_2, headers=header)
     print("Quick hunt status code: {}".format(hunt_response.status_code))
-    if buy_counter == 10:
-        # Send the POST request
-        buy_response = requests.post(
-            "https://bloodlore-chronicles.com/api/game/buysell",
-            headers=headers_4,
-            cookies=cookies_4,
-            data=body_4
-        )
-        print("Buy response status code: {}".format(buy_response.status_code))
-        buy_counter = 0
-    time.sleep(random.randint(620, 650))
+
+    # Set the time to sleep depending on whether blood craze procced or not
+    # Figure out whether blood craze procced or not depending on the next hunt time
+    next_hunt_time = get_next_hunt_time()
+    time_delta = next_hunt_time - int(time.time())
+
+    time.sleep(random.randint(time_delta + 15, time_delta + 45))
+
+# if buy_counter == 10:
+#     # Send the POST request
+#     buy_response = requests.post(
+#         url_3,
+#         headers=header,
+#         data=buy_body
+#     )
+#     print("Buy response status code: {}".format(buy_response.status_code))
+#     buy_counter = 0
